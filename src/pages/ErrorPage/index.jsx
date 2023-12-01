1dd93e1c8e7ae1e0b0f889c0854ae57ed6cc4b44
Tereza Smolova <tereza.smolova@gmail.com>
2023-12-01 16:24:14 +0100

    Repair

--- src/pages/ErrorPage/index.jsx
+++ src/pages/ErrorPage/index.jsx
@@ -5,8 +5,8 @@ export const ErrorPage = () => {
     <main>
       <section className='error'>
           <div className='error__content'>
-          <h1 className='error__title'>404: Tady nic není</h1>
-          <p className='error__text'>Asi jste se spletli</p>
+            <h1 className='error__title'>404: Tady nic není</h1>
+            <p className='error__text'>Asi jste se spletli</p>
           </div>
         </section>
     </main>
