2be0a348779c5771ebdb1f3953490e39d36fea2c
Hedvika Reichlová <hedvika.reichlova@gmail.com>
2023-12-02 09:13:44 +0100

    small_correction

--- src/pages/InfoPage/index.jsx
+++ src/pages/InfoPage/index.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 export const InfoPage = () => {
 	return (
 		<section className='info'>
-			<div className='info__content'>y
+			<div className='info__content'>
 				<h1 className='info__title'>O projektu</h1>
 				<p className='info__text'>
 					Jsme tři kodérky, které chtějí změnit svou kariéru a naučit se něco nového. Náš projekt je
