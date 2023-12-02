491662780f566e129cc5c9328463d0de6dc2f855
Hedvika Reichlová <hedvika.reichlova@gmail.com>
2023-12-02 17:02:52 +0100

    swipebutton_done

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,72 +1,89 @@
 import './style.css'
-import { Link } from 'react-router-dom'
-import { FullscreenButton } from '../FullscreenButton'
+import {FullscreenButton} from '../FullscreenButton'
 import imgSwipeBase from './img/posouvatko_zaklad.svg'
 import imgSwipeHome from './img/posouvatko_domu.svg'
 import imgSwipeInfo from './img/posouvatko_info.svg'
-import { useSwipeable } from 'react-swipeable'
-import { useState, useEffect } from 'react'
+import {useSwipeable} from 'react-swipeable'
+import {useState, useEffect} from 'react'
+import {useNavigate} from 'react-router-dom'
+
+
 
 export const Navigation = () => {
-  const [isToggled, setIsToggled] = useState(false);
-  const [isDesktop, setIsDesktop] = useState(
-    typeof window !== 'undefined' ? window.innerWidth > 768 : false
-  );
+	const navigate = useNavigate()
+	const [isToggled, setIsToggled] = useState({
+    home: false,
+    info: false
+  })
+	const IsTouchScreen = (( 'ontouchstart' in window ) || 
+  ( navigator.maxTouchPoints > 0 ) || 
+  ( navigator.msMaxTouchPoints > 0 ))
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsDesktop(window.innerWidth > 768);
-    };
 
-    if (typeof window !== 'undefined') {
-      window.addEventListener('resize', handleResize);
+	// useEffect(() => {
+  //   setIsTouchScreen()
+	// 	// const handleResize = () => {
+	// 	// 	setIsDesktop(window.innerWidth > 935)
+	// 	// }
+	// 	// if (typeof window !== 'undefined') {
+	// 	// 	window.addEventListener('resize', handleResize)
+	// 	// 	return () => {
+	// 	// 		window.removeEventListener('resize', handleResize)
+	// 	// 	}
+	// 	// }
+	// }, [])
 
-      return () => {
-        window.removeEventListener('resize', handleResize);
-      };
-    }
-  }, []);
 
-  const handleToggle = () => {
-    if (isDesktop)
-    {setIsToggled(true)};
-    console.log ('handle')
-  };
+	const handleEvent = (url) => {
+		setIsToggled({
+      home: url === '/',
+      info: url === '/info'
+    })
+		setTimeout(() => {
+			navigate(url)
+		}, 350)
+	}
+	const handleToggle = (url) => {
+		if (IsTouchScreen) {
+			handleEvent(url)
+		}
+	}
+	const handleSwiped = (url) => {
+		if (!IsTouchScreen) {
+			handleEvent(url)
+		}
+	}
+	const swipeconfig = {preventScrollOnSwipe: false}
+	const handlers1 = useSwipeable({
+		onSwiped: () => handleSwiped('/'),
+		...swipeconfig,
+	})
+	const handlers2 = useSwipeable({
+		onSwiped: () => handleSwiped('/info'),
+		...swipeconfig,
+	})
+  
+	return (
+		<nav>
+			<div className='swipe-button swipe-button--1' {...handlers1} onClick={() => handleToggle('/')}>
+				<img className='swipe-button__base' src={imgSwipeBase} alt='ikona posouvátko'></img>
+				<img
+					className={`swipe-button__home ${isToggled.home ? 'moveLeft' : ''}`}
+					src={imgSwipeHome}
+					alt='ikona domů'
+				></img>
+			</div>
 
-  const handleSwiped = (eventData) => {
-    if (!isDesktop) {
-      console.log("User Swiped!", eventData);
-    }
-  };
-
-  const swipeconfig = { preventScrollOnSwipe: false };
-  const handlers1 = useSwipeable({
-    onSwiped: handleSwiped,
-    ...swipeconfig
-  });
-
-  const handlers2 = useSwipeable({
-    onSwiped: handleSwiped,
-    ...swipeconfig
-  });
-
-  return (
-    <nav>
-      <div className={`swipe-button swipe-button--1 ${isToggled ? 'moveLeft' : ''}`} {...handlers1} onClick={handleToggle}>
-        <img className='swipe-button__base' src={imgSwipeBase} alt='ikona posouvátko'></img>
-        <Link to={'/'}>
-          <img className='swipe-button__home' src={imgSwipeHome} alt='ikona domů'></img>
-        </Link>
-      </div>
-
-      <FullscreenButton />
-
-      <div className={`swipe-button swipe-button--2 ${isToggled ? 'toggled' : ''}`} {...handlers2} onClick={handleToggle}>
-        <img className='swipe-button__base' src={imgSwipeBase} alt='ikona posouvátko'></img>
-        <Link to={'/info'}>
-          <img className='swipe-button__info' src={imgSwipeInfo} alt='ikona info'></img>
-        </Link>
-      </div>
-    </nav>
-  );
-};
+			<FullscreenButton />
+			<div className='swipe-button swipe-button--2' {...handlers2} onClick={() => handleToggle('/info')}>
+				<img
+					className='swipe-button__base'
+					src={imgSwipeBase}
+					alt='ikona posouvátko'
+				></img>
+				<img className={`swipe-button__info ${isToggled.info ? 'moveRight' : ''}`}
+        src={imgSwipeInfo} alt='ikona info'></img>
+			</div>
+		</nav>
+	)
+}
